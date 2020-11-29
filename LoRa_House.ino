a7bc266247cbb02eb5ac41dd692f4e443d41690f
Craig Anderson <cranderson0110@gmail.com>
2020-11-29 15:49:02 +1300

    NTP! Logging! Wow!
    Yeah it's all happening now

--- LoRa_House.ino
+++ LoRa_House.ino
@@ -5,6 +5,7 @@
 #include <WiFi.h>
 #include <ESPmDNS.h>
 
+#include "TimeLib.h"
 #include <WebServer.h>
 #include <ArduinoJson.h>
 
@@ -49,6 +50,13 @@ String password = DEFAULT_PASSWORD;
 WebServer server(80);
 const char* uploadPage = "<form method='POST' action='/update' enctype='multipart/form-data'> <input type='file' name='update'> <input type='submit' value='Update KeepHome'> </form> <br> <form method='GET' action='/updateRemote' enctype='multipart/form-data'> <input type='submit' value='Prepare KeepBox for update'> </form>";
 IPAddress ip;
+
+// NTP
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+static const char ntpServerName[] = "nz.pool.ntp.org";
+//const int timeZone = 12; // UTC+12 (NZST)
+const int timeZone = 13; // UTC+13 (NZDT)
 
 // Various variables
 unsigned int lastMillis = 0;
@@ -228,10 +236,96 @@ void enableRemoteUpgrade () {
   lora = true;
 }
 
+// Long-term logging functions
+void incrementReboots () {
+  time_t t = now();
+  String timeNow = "";
+  timeNow += year(t);
+  timeNow += "/";
+  timeNow += month(t);
+  timeNow += "/";
+  timeNow += day(t);
+  timeNow += " ";
+  timeNow += hour(t);
+  timeNow += ":";
+  timeNow += minute(t);
+  timeNow += ":";
+  int seconds = second(t);
+  if (seconds < 10) {
+    timeNow += "0";
+  }
+  timeNow += seconds;
+  File loggingFile = SPIFFS.open("/log.txt", FILE_APPEND);
+  loggingFile.println("Rebooted at " + timeNow);
+  loggingFile.close();
+}
+
+/*-------- NTP code ----------*/
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime () {
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket (IPAddress &address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
 
 // HTTP POST, the actual point of the unit(s)
 void handleRoot () {
-  server.send(200, "text/plain", "Hello world!"); // Send HTTP status 200 (Ok) and send some text to the browser/client
+  File loggingFile = SPIFFS.open("/log.txt", FILE_READ);
+  String logFile = "\n";
+  while (loggingFile.available()) {
+    logFile += char(loggingFile.read());
+  }
+  loggingFile.close();
+  
+  server.send(200, "text/plain", "Hello world!    Log:" + logFile); // Send HTTP status 200 (Ok) and send some text to the browser/client
 }
 
 void handlePost () {
@@ -543,6 +637,13 @@ void setup () {
   
   // Wait until everything is ready, then send first packet
   vTaskDelay(500);
+
+  // NTP
+  Udp.begin(localPort);
+  setSyncProvider(getNtpTime);
+  setSyncInterval(300); // Seconds between re-sync
+  incrementReboots();
+  
   lastMillis = millis();
   if (lora) {
     displayPayload();
